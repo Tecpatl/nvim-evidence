37c8cf68db57f93cab776032af8160ca904f704a
tecpatl <zldarkist@126.com>
2023-05-29 00:56:36 +0800

    model update

--- lua/evidence/model/index.lua
+++ lua/evidence/model/index.lua
@@ -12,72 +12,57 @@ local tools = require("evidence.util.tools")
 --
 ---@class ModelTableInfo
 ---@field uri string
----@field all_table table<string,Parameters|{}>
----@field now_table_id string
+---@field paramter Parameters
 
 ---@class Model
 ---@field tbl SqlTable
 ---@field is_setup boolean
----@field now_table_id number
+---@field paramter Parameters
 ---@field instance Model
----@field all_table table<string,Parameters>
 local Model = {}
 
 Model.__index = function(self, key)
-	local value = rawget(Model, key)
-	if key ~= "setup" then
-		if not self.is_setup then
-			error("Class not initialized. Please call setup() first.", 2)
-		end
-	end
-	return value
+  local value = rawget(Model, key)
+  if key ~= "setup" then
+    if not self.is_setup then
+      error("Class not initialized. Please call setup() first.", 2)
+    end
+  end
+  return value
 end
 
 Model.__newindex = function()
-	error("Attempt to modify a read-only table")
+  error("Attempt to modify a read-only table")
 end
 
 function Model:getInstance()
-	if not self.instance then
-		self.tbl = SqlTable:new()
-		self.all_table = {}
-		self.instance = setmetatable({ is_setup = false }, self)
-	end
-	return self.instance
+  if not self.instance then
+    self.tbl = SqlTable:new()
+    self.paramter = {}
+    self.instance = setmetatable({ is_setup = false }, self)
+  end
+  return self.instance
 end
 
 ---@param data ModelTableInfo
 function Model:setup(data)
-	if self.is_setup then
-		error("cannot setup twice")
-	end
-	self.is_setup = true
-	local all_id = {}
-	for key, item in pairs(data.all_table) do
-		self.all_table[key] = _.Parameters:new(item)
-		table.insert(all_id, key)
-	end
-	local sql_info = {
-		uri = data.uri,
-		now_table_id = data.now_table_id,
-		all_table_id = all_id,
-	}
-	self.tbl:setup(sql_info)
-end
-
----@return table<string>
-function Model:getTableIds()
-	return self.tbl:getTableIds()
+  if self.is_setup then
+    error("cannot setup twice")
+  end
+  self.is_setup = true
+  self.paramter = _.Parameters:new(data.paramter)
+  local sql_info = {
+    uri = data.uri,
+  }
+  self.tbl:setup(sql_info)
 end
 
 ---@return ModelTableInfo
 function Model:getAllInfo()
-	return {
-		uri = self.tbl.uri,
-		now_table_id = self.tbl.now_table_id,
-		all_table = self.all_table,
-		sql_table = self.tbl:dump(),
-	}
+  return {
+    uri = self.tbl.uri,
+    sql_table = self.tbl:dump(),
+  }
 end
 
 --function Model:release()
@@ -88,131 +73,123 @@ end
 
 ---@param content string
 function Model:addNewCard(content)
-	local card_info = _.Card:new()
-	local file_type = vim.bo.filetype
-	if not file_type or file_type == "" then
-		file_type = "markdown"
-	end
+  local card_info = _.Card:new()
+  local file_type = vim.bo.filetype
+  if not file_type or file_type == "" then
+    file_type = "markdown"
+  end
 
-	self.tbl:insertCard(content, card_info:dumpStr(), card_info.due, file_type)
+  self.tbl:insertCard(content, card_info:dumpStr(), card_info.due, file_type)
 end
 
 ---@param id number
 ---@param row table
----@return boolean
 function Model:editCard(id, row)
-	return self.tbl:editById(id, row)
+  self.tbl:editCard(id, row)
 end
 
----@return nil | FsrsTableField[]
+---@return nil | CardField[]
 function Model:findAll()
-	return self.tbl:find(-1, nil)
+  return self.tbl:findCard()
 end
 
 ---@param limit_num? number
 ---@return CardItem[]|nil
 function Model:getMinDueItem(limit_num)
-	local item = self.tbl:min("due", "info NOT LIKE '%reps=0%'", limit_num)
+  local item = self.tbl:minCard("due", "info NOT LIKE '%reps=0%'", limit_num)
   return self:convertFsrsTableField2CardItem(item)
 end
 
 ---@param limit_num number
 ---@return CardItem[]|nil
 function Model:getNewItem(limit_num)
-	local item = self.tbl:find(limit_num, "info LIKE '%reps=0%'")
+  local item = self.tbl:findCard(limit_num, "info LIKE '%reps=0%'")
   return self:convertFsrsTableField2CardItem(item)
 end
 
 ---@param id number
----@return FsrsTableField
+---@return CardField
 function Model:findById(id)
-	local ret = self.tbl:find(1, "id=" .. id)
-	if ret ~= nil then
-		return ret[1]
-	else
-		error("findById not exist id:" .. id)
-	end
+  local ret = self.tbl:findCard(1, "id=" .. id)
+  if ret ~= nil then
+    return ret[1]
+  else
+    error("findById not exist id:" .. id)
+  end
 end
 
 ---@param content string
 ---@param lim? number
 ---@return CardItem[]|nil
 function Model:fuzzyFind(content, lim)
-	lim = lim or 10
+  lim = lim or 10
   local item = nil
-	if content ~= "" then
-		item = self.tbl:find(lim, "content like '%" .. content .. "%'")
-	else
-		item = self.tbl:find(lim)
-	end
+  if content ~= "" then
+    item = self.tbl:findCard(lim, "content like '%" .. content .. "%'")
+  else
+    item = self.tbl:findCard(lim)
+  end
   return self:convertFsrsTableField2CardItem(item)
 end
 
----@param item FsrsTableField[] | nil
+---@param item CardField[] | nil
 ---@return CardItem[]|nil
 function Model:convertFsrsTableField2CardItem(item)
-	if item ~= nil then
-		local arr = {}
-		for _, v in ipairs(item) do
-			table.insert(arr, {
-				id = v.id,
-				content = v.content,
-				due = v.due,
-				file_type = v.file_type,
-				card = self:convertRealCard(v),
-			})
-		end
-		return arr
-	else
-		return nil
-	end
+  if item ~= nil then
+    local arr = {}
+    for _, v in ipairs(item) do
+      table.insert(arr, {
+        id = v.id,
+        content = v.content,
+        due = v.due,
+        file_type = v.file_type,
+        card = self:convertRealCard(v),
+      })
+    end
+    return arr
+  else
+    return nil
+  end
 end
 
 ---@param id number
 function Model:delCard(id)
-	self.tbl:del(id)
-	return true
-end
-
----@param table_id string
----@return boolean
-function Model:switchTable(table_id)
-	return self.tbl:setTable(table_id)
+  self.tbl:delCard(id)
 end
 
 ---@return Parameters
 function Model:getParameter()
-	return self.all_table[self.now_table_id]
+  return self.paramter
 end
 
 ---@return FSRS
 function Model:getFsrs()
-	return _FSRS_.fsrs:new(self:getParameter())
+  return _FSRS_.fsrs:new(self:getParameter())
 end
 
-function Model:clear()
-	return self.tbl:clear()
-end
+--function Model:clear()
+--return self.tbl:clear()
+--end
 
----@param sql_card FsrsTableField
+---@param sql_card CardField
 ---@return Card
 function Model:convertRealCard(sql_card)
-	local data = tools.parse(sql_card.info)
-	--print(vim.inspect(data))
-	return _.Card:new(data)
+  local data = tools.parse(sql_card.info)
+  --print(vim.inspect(data))
+  return _.Card:new(data)
 end
 
 ---@param id number
 ---@param rating RatingType
 ---@param now_time? Timestamp
 function Model:ratingCard(id, rating, now_time)
-	local fsrs = self:getFsrs()
-	local sql_card = self:findById(id)
-	now_time = now_time or os.time()
-	local new_card = fsrs:repeats(self:convertRealCard(sql_card), now_time)[rating].card
-	sql_card.due = new_card.due
-	sql_card.info = new_card:dumpStr()
-	self:editCard(id, sql_card)
+  local fsrs = self:getFsrs()
+  local sql_card = self:findById(id)
+  now_time = now_time or os.time()
+  local new_card = fsrs:repeats(self:convertRealCard(sql_card), now_time)[rating].card
+  sql_card.due = new_card.due
+  sql_card.info = new_card:dumpStr()
+  self:editCard(id, sql_card)
 end
 
 return Model:getInstance()
