21bf9eade69be792fb608f42273d437df527ff4a
tecpatl <zldarkist@126.com>
2023-06-04 14:33:39 +0800

    update readme && remove hydra

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -3,7 +3,7 @@ vim.notify = print
 vim.opt.rtp:append(".")
 vim.opt.rtp:append(lazypath .. "/nvim-treesitter")
 vim.opt.rtp:append(lazypath .. "/sqlite.lua")
-vim.opt.rtp:append(lazypath .. "/hydra.nvim")
+--vim.opt.rtp:append(lazypath .. "/hydra.nvim")
 vim.opt.rtp:append(lazypath .. "/telescope.nvim")
 vim.opt.swapfile = false
 A = function(...)
